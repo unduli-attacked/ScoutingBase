74890de6e732907793fcb74e4906ab1daf5e8bb2
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2020-03-13 16:22:17 -0700

    Main.java

--- src/main/java/base/Main.java
+++ src/main/java/base/Main.java
@@ -23,20 +23,59 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main{
+public class Main {
+    /**
+     * The name of the current event, set when a session is selected
+     */
     public static String eventName = "";
+    
+    /**
+     * The current Session for use throughout the project, set when a session is selected
+     */
     public static Session currentSession;
+    
+    /**
+     * All Sessions recovered from .json files in main_storage. Filled when the UI is launched
+     */
     public static ArrayList<Session> recoveredSessions = new ArrayList<>();
+    
+    /**
+     * Knockoff-singleton MatchCollectionThread for use throughout the project.
+     */
     public static MatchCollationThread matchCollationThread;
+    
+    /**
+     * Knockoff-singleton MatchCollectionThread for use throughout the project.
+     */
     public static PitCollectionThread pitCollectionThread;
+    
+    /**
+     * Knockoff-singleton PitCollectionThread for use throughout the project.
+     */
     public static TeamCollationThread teamCollationThread;
+    
+    /**
+     * Knockoff-singleton TeamCollationThread for use throughout the project.
+     */
     public static TBACollectionThread tbaCollectionThread;
+    
+    /**
+     * Set to {@code true} when {@code tbaCollectionThread} is started for the first time
+     */
     public static boolean tbaIsSync;
+    
+    /**
+     * The Base's instance of {@link TBA} for use throughout the project. Set in {@code main} on startup.
+     */
     public static TBA tbaApi;
     
-    
+    /**
+     * Launches the program. Sets the auth token for {@link TBA}, recovers sessions, and launches {@link Entry}
+     * @param args console arguments
+     * @throws IOException thrown from session recovery
+     */
     public static void main(String args[]) throws IOException {
-    
+        
         TBA.setAuthToken("OPynqKt8K0vueAXqxZzdigY9OBYK3KMgQQrsM4l8jE5cBmGfByhy6YzVIb2Ts7xD");
         tbaApi = new TBA();
         
@@ -45,15 +84,16 @@ public class Main{
         new File("main_storage/admins/").mkdir();
         
         File sessions = new File("main_storage/sessions/");
-        if(sessions.isDirectory() && sessions.listFiles()!=null){
-            for(File fl : sessions.listFiles()){
+        if (sessions.isDirectory() && sessions.listFiles() != null) {
+            for (File fl : sessions.listFiles()) {
                 recoveredSessions.add(SavingFunctions.recoverFullSession(fl));
             }
         }
-    
-    
+        
+        
         Application.launch(Entry.class, args);
     }
     
-    public Main(){}
+    public Main() {
+    }
 }
